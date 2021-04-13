fe802a274fc46c112cc3b01b27be6a922a634867
Diganta Som <itsdsom111@gmail.com>
2021-04-13 23:54:08 +0530

    Fixed a small login issue

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { emailSignInStart, googleSignInStart } from '../../redux/user/user.actions';
+import { selectUserError } from '../../redux/user/user.selectors';
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 
-const SignIn = ({ googleSignInStart, emailSignInStart }) => {
+const SignIn = ({ googleSignInStart, emailSignInStart, error }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -24,6 +26,11 @@ const SignIn = ({ googleSignInStart, emailSignInStart }) => {
     e.preventDefault();
     emailSignInStart(email, password);
   };
+
+  // TODO: fix it so that every time sign in failure action fires, this alert shows each time as well.
+  if (error) {
+    alert(error);
+  }
 
   return (
     <div className='sign-in'>
@@ -60,9 +67,13 @@ const SignIn = ({ googleSignInStart, emailSignInStart }) => {
   );
 };
 
+const mapStateToProps = createStructuredSelector({
+  error: selectUserError
+});
+
 const mapDispatchToProps = dispatch => ({
   googleSignInStart: () => dispatch(googleSignInStart()),
   emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
 });
 
-export default connect(null, mapDispatchToProps)(SignIn);
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
