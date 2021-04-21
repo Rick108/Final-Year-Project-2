4382dcd0c0b9f5e902e2ce7bce6355b08ab5f2c1
Diganta Som <itsdsom111@gmail.com>
2021-04-21 11:11:31 +0530

    Fixed external links in profile top component

--- src/components/profile-top/profile-top.component.jsx
+++ src/components/profile-top/profile-top.component.jsx
@@ -27,32 +27,32 @@ const ProfileTop = ({
       {location && <p>{location}</p>}
       <div className='icons'>
         {website && (
-          <a href={website}>
+          <a href={`https://${website}`} target='_blank' rel='noreferrer'>
             <i className='fas fa-globe fa-2x'></i>
           </a>
         )}
         {twitter && (
-          <a href={twitter}>
+          <a href={`https://${twitter}`} target='_blank' rel='noreferrer'>
             <i className='fab fa-twitter fa-2x'></i>
           </a>
         )}
         {linkedin && (
-          <a href={linkedin}>
+          <a href={`https://${linkedin}`} target='_blank' rel='noreferrer'>
             <i className='fab fa-linkedin fa-2x'></i>
           </a>
         )}
         {youtube && (
-          <a href={youtube}>
+          <a href={`https://${youtube}`} target='_blank' rel='noreferrer'>
             <i className='fab fa-youtube fa-2x'></i>
           </a>
         )}
         {facebook && (
-          <a href={facebook}>
+          <a href={`https://${facebook}`} target='_blank' rel='noreferrer'>
             <i className='fab fa-facebook fa-2x'></i>
           </a>
         )}
         {instagram && (
-          <a href={instagram}>
+          <a href={`https://${instagram}`}>
             <i className='fab fa-instagram fa-2x'></i>
           </a>
         )}
